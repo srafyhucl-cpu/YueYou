22f17849a9a1cc0786a830007657ba8e1e7d38d8
srafy <srafy@example.com>
2026-04-03 13:42:54 +0800

    补充测试优化相关文件

--- fix_tests.dart
+++ fix_tests.dart
@@ -8,70 +8,69 @@ void main() {
 
   for (final file in files) {
     var content = File(file).readAsStringSync();
-    
+
     // 替换 p.board = List.generate(...)
     content = content.replaceAllMapped(
-      RegExp(r'p\.board\s*=\s*List\.generate\([^;]+;\s*p\.board\[0\]\[0\]\s*=\s*(const TileModel\([^)]+\));\s*p\.board\[1\]\[1\]\s*=\s*(const TileModel\([^)]+\));'),
-      (match) => '''p.setStateForTesting(board: [
+        RegExp(
+            r'p\.board\s*=\s*List\.generate\([^;]+;\s*p\.board\[0\]\[0\]\s*=\s*(const TileModel\([^)]+\));\s*p\.board\[1\]\[1\]\s*=\s*(const TileModel\([^)]+\));'),
+        (match) => '''p.setStateForTesting(board: [
         [\${match[1]}, null, null, null],
         [null, \${match[2]}, null, null],
         [null, null, null, null],
         [null, null, null, null],
-      ]);'''
-    );
-    
+      ]);''');
+
     // 替换 p.board[0][1] = ...; p.board[0][2] = ...;
     content = content.replaceAllMapped(
-      RegExp(r'p\.board\[0\]\[1\]\s*=\s*(const TileModel\([^)]+\));\s*p\.board\[0\]\[2\]\s*=\s*(const TileModel\([^)]+\));'),
-      (match) => '''p.setStateForTesting(board: [
+        RegExp(
+            r'p\.board\[0\]\[1\]\s*=\s*(const TileModel\([^)]+\));\s*p\.board\[0\]\[2\]\s*=\s*(const TileModel\([^)]+\));'),
+        (match) => '''p.setStateForTesting(board: [
         [null, \${match[1]}, \${match[2]}, null],
         [null, null, null, null],
         [null, null, null, null],
         [null, null, null, null],
-      ]);'''
-    );
-    
+      ]);''');
+
     // 替换 p.board[0][0] = ...; p.board[0][1] = ...;
     content = content.replaceAllMapped(
-      RegExp(r'p\.board\[0\]\[0\]\s*=\s*(const TileModel\([^)]+\));\s*p\.board\[0\]\[1\]\s*=\s*(const TileModel\([^)]+\));'),
-      (match) => '''p.setStateForTesting(board: [
+        RegExp(
+            r'p\.board\[0\]\[0\]\s*=\s*(const TileModel\([^)]+\));\s*p\.board\[0\]\[1\]\s*=\s*(const TileModel\([^)]+\));'),
+        (match) => '''p.setStateForTesting(board: [
         [\${match[1]}, \${match[2]}, null, null],
         [null, null, null, null],
         [null, null, null, null],
         [null, null, null, null],
-      ]);'''
-    );
-    
+      ]);''');
+
     // 替换 square_board_test 的赋值
     content = content.replaceAllMapped(
-      RegExp(r'provider\.board\s*=\s*List\.generate\([^;]+;\s*provider\.board\[1\]\[1\]\s*=\s*(const TileModel\([^)]+\));'),
-      (match) => '''provider.setStateForTesting(board: [
+        RegExp(
+            r'provider\.board\s*=\s*List\.generate\([^;]+;\s*provider\.board\[1\]\[1\]\s*=\s*(const TileModel\([^)]+\));'),
+        (match) => '''provider.setStateForTesting(board: [
         [null, null, null, null],
         [null, \${match[1]}, null, null],
         [null, null, null, null],
         [null, null, null, null],
-      ]);'''
-    );
-    
+      ]);''');
+
     content = content.replaceAllMapped(
-      RegExp(r'provider\.board\s*=\s*List\.generate\([^;]+;\s*provider\.board\[2\]\[2\]\s*=\s*(const TileModel\([^)]+\));\s*provider\.board\[2\]\[3\]\s*=\s*(const TileModel\([^)]+\));'),
-      (match) => '''provider.setStateForTesting(board: [
+        RegExp(
+            r'provider\.board\s*=\s*List\.generate\([^;]+;\s*provider\.board\[2\]\[2\]\s*=\s*(const TileModel\([^)]+\));\s*provider\.board\[2\]\[3\]\s*=\s*(const TileModel\([^)]+\));'),
+        (match) => '''provider.setStateForTesting(board: [
         [null, null, null, null],
         [null, null, null, null],
         [null, null, \${match[1]}, \${match[2]}],
         [null, null, null, null],
-      ]);'''
-    );
+      ]);''');
 
     content = content.replaceAllMapped(
-      RegExp(r'provider\.isOver\s*=\s*(true|false);\s*provider\.score\s*=\s*(\d+);\s*provider\.board\s*=\s*List\.generate\([^;]+;'),
-      (match) => 'provider.setStateForTesting(isOver: \${match[1]}, score: \${match[2]});'
-    );
-    
-    content = content.replaceAll(
-      RegExp(r'provider\.isOver\s*=\s*true;'),
-      'provider.setStateForTesting(isOver: true);'
-    );
+        RegExp(
+            r'provider\.isOver\s*=\s*(true|false);\s*provider\.score\s*=\s*(\d+);\s*provider\.board\s*=\s*List\.generate\([^;]+;'),
+        (match) =>
+            'provider.setStateForTesting(isOver: \${match[1]}, score: \${match[2]});');
+
+    content = content.replaceAll(RegExp(r'provider\.isOver\s*=\s*true;'),
+        'provider.setStateForTesting(isOver: true);');
 
     File(file).writeAsStringSync(content);
   }
--- fix_tests_new.dart
+++ fix_tests_new.dart
@@ -8,72 +8,102 @@ void main() {
 
   for (final file in files) {
     var content = File(file).readAsStringSync();
-    
+
     content = content.replaceAllMapped(
-      RegExp(r'p\.board\s*=\s*List\.generate\([^;]+;\s*p\.board\[0\]\[0\]\s*=\s*(const TileModel\([^)]+\));\s*p\.board\[1\]\[1\]\s*=\s*(const TileModel\([^)]+\));'),
-      (match) => '''p.setStateForTesting(board: [
-        [''' + match.group(1)! + ''', null, null, null],
-        [null, ''' + match.group(2)! + ''', null, null],
+        RegExp(
+            r'p\.board\s*=\s*List\.generate\([^;]+;\s*p\.board\[0\]\[0\]\s*=\s*(const TileModel\([^)]+\));\s*p\.board\[1\]\[1\]\s*=\s*(const TileModel\([^)]+\));'),
+        (match) =>
+            '''p.setStateForTesting(board: [
+        [''' +
+            match.group(1)! +
+            ''', null, null, null],
+        [null, ''' +
+            match.group(2)! +
+            ''', null, null],
         [null, null, null, null],
         [null, null, null, null],
-      ]);'''
-    );
-    
+      ]);''');
+
     content = content.replaceAllMapped(
-      RegExp(r'p\.board\[0\]\[1\]\s*=\s*(const TileModel\([^)]+\));\s*p\.board\[0\]\[2\]\s*=\s*(const TileModel\([^)]+\));'),
-      (match) => '''p.setStateForTesting(board: [
-        [null, ''' + match.group(1)! + ''', ''' + match.group(2)! + ''', null],
+        RegExp(
+            r'p\.board\[0\]\[1\]\s*=\s*(const TileModel\([^)]+\));\s*p\.board\[0\]\[2\]\s*=\s*(const TileModel\([^)]+\));'),
+        (match) =>
+            '''p.setStateForTesting(board: [
+        [null, ''' +
+            match.group(1)! +
+            ''', ''' +
+            match.group(2)! +
+            ''', null],
         [null, null, null, null],
         [null, null, null, null],
         [null, null, null, null],
-      ]);'''
-    );
-    
+      ]);''');
+
     content = content.replaceAllMapped(
-      RegExp(r'p\.board\[0\]\[0\]\s*=\s*(const TileModel\([^)]+\));\s*p\.board\[0\]\[1\]\s*=\s*(const TileModel\([^)]+\));'),
-      (match) => '''p.setStateForTesting(board: [
-        [''' + match.group(1)! + ''', ''' + match.group(2)! + ''', null, null],
+        RegExp(
+            r'p\.board\[0\]\[0\]\s*=\s*(const TileModel\([^)]+\));\s*p\.board\[0\]\[1\]\s*=\s*(const TileModel\([^)]+\));'),
+        (match) =>
+            '''p.setStateForTesting(board: [
+        [''' +
+            match.group(1)! +
+            ''', ''' +
+            match.group(2)! +
+            ''', null, null],
         [null, null, null, null],
         [null, null, null, null],
         [null, null, null, null],
-      ]);'''
-    );
+      ]);''');
 
     // 对于一些直接赋值的 p.board = [ ... ]; 换成 setStateForTesting
     content = content.replaceAllMapped(
-      RegExp(r'p\.board\s*=\s*(\[[^;]+\]);', multiLine: true),
-      (match) => 'p.setStateForTesting(board: ' + match.group(1)! + ');'
-    );
+        RegExp(r'p\.board\s*=\s*(\[[^;]+\]);', multiLine: true),
+        (match) => 'p.setStateForTesting(board: ' + match.group(1)! + ');');
 
     content = content.replaceAllMapped(
-      RegExp(r'provider\.board\s*=\s*List\.generate\([^;]+;\s*provider\.board\[1\]\[1\]\s*=\s*(const TileModel\([^)]+\));'),
-      (match) => '''provider.setStateForTesting(board: [
+        RegExp(
+            r'provider\.board\s*=\s*List\.generate\([^;]+;\s*provider\.board\[1\]\[1\]\s*=\s*(const TileModel\([^)]+\));'),
+        (match) =>
+            '''provider.setStateForTesting(board: [
         [null, null, null, null],
-        [null, ''' + match.group(1)! + ''', null, null],
-        [null, null, null, null],
-        [null, null, null, null],
-      ]);'''
-    );
-    
-    content = content.replaceAllMapped(
-      RegExp(r'provider\.board\s*=\s*List\.generate\([^;]+;\s*provider\.board\[2\]\[2\]\s*=\s*(const TileModel\([^)]+\));\s*provider\.board\[2\]\[3\]\s*=\s*(const TileModel\([^)]+\));'),
-      (match) => '''provider.setStateForTesting(board: [
+        [null, ''' +
+            match.group(1)! +
+            ''', null, null],
         [null, null, null, null],
         [null, null, null, null],
-        [null, null, ''' + match.group(1)! + ''', ''' + match.group(2)! + '''],
-        [null, null, null, null],
-      ]);'''
-    );
+      ]);''');
 
     content = content.replaceAllMapped(
-      RegExp(r'provider\.isOver\s*=\s*(true|false);\s*provider\.score\s*=\s*(\d+);\s*provider\.board\s*=\s*(\[[^;]+\]);', multiLine: true),
-      (match) => 'provider.setStateForTesting(isOver: ' + match.group(1)! + ', score: ' + match.group(2)! + ', board: ' + match.group(3)! + ');'
-    );
+        RegExp(
+            r'provider\.board\s*=\s*List\.generate\([^;]+;\s*provider\.board\[2\]\[2\]\s*=\s*(const TileModel\([^)]+\));\s*provider\.board\[2\]\[3\]\s*=\s*(const TileModel\([^)]+\));'),
+        (match) =>
+            '''provider.setStateForTesting(board: [
+        [null, null, null, null],
+        [null, null, null, null],
+        [null, null, ''' +
+            match.group(1)! +
+            ''', ''' +
+            match.group(2)! +
+            '''],
+        [null, null, null, null],
+      ]);''');
 
     content = content.replaceAllMapped(
-      RegExp(r'provider\.isOver\s*=\s*(true|false);'),
-      (match) => 'provider.setStateForTesting(isOver: ' + match.group(1)! + ');'
-    );
+        RegExp(
+            r'provider\.isOver\s*=\s*(true|false);\s*provider\.score\s*=\s*(\d+);\s*provider\.board\s*=\s*(\[[^;]+\]);',
+            multiLine: true),
+        (match) =>
+            'provider.setStateForTesting(isOver: ' +
+            match.group(1)! +
+            ', score: ' +
+            match.group(2)! +
+            ', board: ' +
+            match.group(3)! +
+            ');');
+
+    content = content.replaceAllMapped(
+        RegExp(r'provider\.isOver\s*=\s*(true|false);'),
+        (match) =>
+            'provider.setStateForTesting(isOver: ' + match.group(1)! + ');');
 
     File(file).writeAsStringSync(content);
   }
