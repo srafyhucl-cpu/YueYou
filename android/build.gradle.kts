27f66287863d69f01f4185a248402752b8d8a9f5
srafy <srafy@example.com>
2026-07-12 00:00:40 +0800

    fix(release): 恢复 arm64 正式构建

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,5 +1,8 @@
 allprojects {
     repositories {
+        maven { url = uri("https://repo.huaweicloud.com/repository/maven/") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
         google()
         mavenCentral()
     }
@@ -16,6 +19,39 @@ subprojects {
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
 subprojects {
+    plugins.withId("com.android.application") {
+        extensions.configure<com.android.build.gradle.AppExtension>("android") {
+            compileOptions {
+                sourceCompatibility = JavaVersion.VERSION_17
+                targetCompatibility = JavaVersion.VERSION_17
+            }
+        }
+    }
+    plugins.withId("com.android.library") {
+        extensions.configure<com.android.build.gradle.LibraryExtension>("android") {
+            compileOptions {
+                sourceCompatibility = JavaVersion.VERSION_17
+                targetCompatibility = JavaVersion.VERSION_17
+            }
+        }
+    }
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        compilerOptions {
+            languageVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_1_8)
+            apiVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_1_8)
+            jvmTarget.set(
+                if (project.name in setOf("audioplayers_android", "flutter_tts", "rive_common", "sentry_flutter")) {
+                    org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8
+                } else {
+                    org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+                }
+            )
+        }
+    }
+    tasks.withType<JavaCompile>().configureEach {
+        sourceCompatibility = JavaVersion.VERSION_17.toString()
+        targetCompatibility = JavaVersion.VERSION_17.toString()
+    }
     project.evaluationDependsOn(":app")
 }
 
